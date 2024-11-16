f1e43f1d27245c9f303d7de2b3a2671f7b137cad
danylosant <do@digiants.com.ua>
2024-11-16 19:43:37 +0200

    complete task functionality implemented

--- src/app.js
+++ src/app.js
@@ -91,7 +91,7 @@ App = {
             $newTaskTemplate.find('input')
                             .prop('name', taskId)
                             .prop('checked', taskCompleted)
-                            // .on('click', App.toggleCompleted)
+                            .on('click', App.toggleCompleted)
         
             // Put the task in the correct list
             if (taskCompleted) {
@@ -103,6 +103,20 @@ App = {
             // Show the task
             $newTaskTemplate.show()
         }
+    },
+
+    createTask: async () => {
+        App.setLoading(true)
+        const content = $('#newTask').val()
+        await App.todoList.createTask(content, { from: App.account })
+        window.location.reload()
+    },
+
+    toggleCompleted: async (e) => {
+        App.setLoading(true)
+        const taskId = e.target.name
+        await App.todoList.toggleCompleted(taskId, { from: App.account })
+        window.location.reload()
     },
 
     setLoading: (boolean) => {
